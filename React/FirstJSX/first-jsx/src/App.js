96f44e599b9ad83374c6cd5f51ab6b31ad9b83cf
Nathan Langione <nlangione@wayfair.com>
2021-10-31 08:50:19 -0400

    First jsx completed

---
+++ React/FirstJSX/first-jsx/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello Dojo!</h1>
+			<h2>Things I need to do:</h2>
+			<div>
+				<ul>
+					<li>Learn React</li>
+					<li>Climb Mt. Everest</li>
+					<li>Run a marathon</li>
+					<li>Feed the dogs</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default App;
