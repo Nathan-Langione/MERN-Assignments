b3d4ad78ee0a905be88820ad2ec1f70572a7ea66
Nathan Langione <nlangione@wayfair.com>
2021-11-07 16:01:16 -0500

    Need to finish pw validations

--- React/HookForm/hook-form/src/components/Form.js
+++ React/HookForm/hook-form/src/components/Form.js
@@ -2,70 +2,105 @@ import React, { useState } from "react";
 
 const Form = (props) => {
 	const [firstName, setFirstName] = useState("");
+	const [firstNameError, setFirstNameError] = useState("");
 	const [lastName, setLastName] = useState("");
+	const [lastNameError, setLastNameError] = useState("");
 	const [email, setEmail] = useState("");
+	const [emailError, setEmailError] = useState("");
 	const [password, setPassword] = useState("");
+	const [passwordError, setPasswordError] = useState("");
 	const [confPassword, setConfPassword] = useState("");
+	const [confPasswordError, setConfPasswordError] = useState("");
 
-	const createUser = (e) => {
-		// we must prevent the default refresh of the browser to keep our state from being reset
-		e.preventDefault();
+	const handleFirstName = (e) => {
+		setFirstName(e.target.value);
+		if (e.target.value.length < 2) {
+			setFirstNameError("First Name must be 2 characters or longer!");
+		} else {
+			// an empty string is considered a "falsy" value
+			setFirstNameError("");
+		}
+	};
 
-		// shorthand ES6 syntax for building an object - see notes above
-		const newUser = { firstName, lastName, email, password, confPassword };
-		console.log("Welcome", newUser);
-		setFirstName("");
-		setLastName("");
-		setEmail("");
-		setPassword("");
-		setConfPassword("");
+	const handleLastName = (e) => {
+		setLastName(e.target.value);
+		if (e.target.value.length < 2) {
+			setLastNameError("Last name must be 2 characters or longer!");
+		} else {
+			// an empty string is considered a "falsy" value
+			setLastNameError("");
+		}
+	};
+
+	const handleEmail = (e) => {
+		setEmail(e.target.value);
+		if (e.target.value.length < 5) {
+			setEmailError("Email must be 5 characters or longer!");
+		} else {
+			// an empty string is considered a "falsy" value
+			setEmailError("");
+		}
+	};
+
+	const handlePassword = (e) => {
+		setPassword(e.target.value);
+		if (e.target.value.length < 8) {
+			setPasswordError("Password must be 8 characters or longer!");
+		} else if (e.target.value !== confPassword) {
+			setPasswordError("Password must match confirmation password");
+		} else {
+			// an empty string is considered a "falsy" value
+			setPasswordError("");
+		}
+	};
+
+	const handleConfPassword = (e) => {
+		setConfPassword(e.target.value);
+		if (e.target.value.length < 8) {
+			setConfPasswordError("Password must be 8 characters or longer!");
+		} else {
+			// an empty string is considered a "falsy" value
+			setConfPasswordError("");
+		}
 	};
 
 	return (
 		<div>
-			<form onSubmit={createUser}>
+			{/* Form to take user input */}
+			<form onSubmit={(e) => e.preventDefault()}>
 				<div>
 					<label>First Name: </label>
-					<input
-						type="text"
-						value={firstName}
-						onChange={(e) => setFirstName(e.target.value)}
-					/>
+					<input type="text" onChange={handleFirstName} />
+					{firstNameError ? <p>{firstNameError}</p> : ""}
 				</div>
 				<div>
 					<label>Last Name: </label>
-					<input
-						type="text"
-						value={lastName}
-						onChange={(e) => setLastName(e.target.value)}
-					/>
+					<input type="text" onChange={handleLastName} />
+					{lastNameError ? <p>{lastNameError}</p> : ""}
 				</div>
 				<div>
 					<label>Email Address: </label>
-					<input
-						type="text"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
+					<input type="text" onChange={handleEmail} />
+					{emailError ? <p>{emailError}</p> : ""}
 				</div>
 				<div>
 					<label>Password: </label>
-					<input
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
+					<input type="password" onChange={handlePassword} />
+					{passwordError ? <p>{passwordError}</p> : ""}
 				</div>
 				<div>
 					<label>Confirm Password: </label>
-					<input
-						type="password"
-						value={confPassword}
-						onChange={(e) => setConfPassword(e.target.value)}
-					/>
+					<input type="password" onChange={handleConfPassword} />
+					{confPasswordError ? <p>{confPasswordError}</p> : ""}
 				</div>
-				<input type="submit" value="Create User" />
+				{firstNameError ? (
+					<input type="submit" value="Create User" disabled />
+				) : (
+					<input type="submit" value="Create User" />
+				)}
 			</form>
+
+			{/* Display form data in real time */}
 			<div>
 				<h1>Your Form Data</h1>
 				<p>First Name {firstName}</p>
