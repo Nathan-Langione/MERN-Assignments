ad981d713aabf90326b8a6675b8c05314f5550bb
Nathan Langione <nlangione@wayfair.com>
2021-11-21 18:49:27 -0500

    working on axios

--- React/AxiosPokemonAPI/axios-pokemon-api/src/App.js
+++ React/AxiosPokemonAPI/axios-pokemon-api/src/App.js
@@ -1,21 +1,35 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-	const [pokemon, setpokemon] = useState([]);
+	/* 	const [pokemon, setpokemon] = useState([]);
 
 	useEffect(() => {
 		fetch("https://pokeapi.co/api/v2/pokemon/?limit=810")
 			.then((response) => response.json())
 			.then((response) => setpokemon(response.results));
+	}, []); */
+
+	const [responseData, setResponseData] = useState(null);
+	useEffect(() => {
+		axios
+			.get("https://pokeapi.co/api/v2/pokemon/?limit=810")
+			.then((response) => {
+				setResponseData(response.data);
+			});
 	}, []);
 
 	return (
 		<div className="App">
-			{pokemon.length > 0 &&
+			{responseData.map((aPokemon, index) => {
+				return <div key={index}>{aPokemon.name}</div>;
+			})}
+
+			{/* 	{pokemon.length > 0 &&
 				pokemon.map((aPokemon, index) => {
 					return <div key={index}>{aPokemon.name}</div>;
-				})}
+				})} */}
 		</div>
 	);
 }
