74cb835dab65ea5a049a3dee65c3694961356319
Nathan Langione <nlangione@wayfair.com>
2021-11-21 18:31:18 -0500

    updated naming

--- React/AxiosPokemonAPI/axios-pokemon-api/src/App.js
+++ React/AxiosPokemonAPI/axios-pokemon-api/src/App.js
@@ -2,19 +2,19 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 
 function App() {
-	const [people, setPeople] = useState([]);
+	const [pokemon, setpokemon] = useState([]);
 
 	useEffect(() => {
 		fetch("https://pokeapi.co/api/v2/pokemon/?limit=810")
 			.then((response) => response.json())
-			.then((response) => setPeople(response.results));
+			.then((response) => setpokemon(response.results));
 	}, []);
 
 	return (
-		<div>
-			{people.length > 0 &&
-				people.map((person, index) => {
-					return <div key={index}>{person.name}</div>;
+		<div className="App">
+			{pokemon.length > 0 &&
+				pokemon.map((aPokemon, index) => {
+					return <div key={index}>{aPokemon.name}</div>;
 				})}
 		</div>
 	);
