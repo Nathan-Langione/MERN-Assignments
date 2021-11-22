4235feb0f7ac008630cd319c8801321e2d133768
Nathan Langione <nlangione@wayfair.com>
2021-11-21 19:00:13 -0500

    completed axios

--- React/AxiosPokemonAPI/axios-pokemon-api/src/App.js
+++ React/AxiosPokemonAPI/axios-pokemon-api/src/App.js
@@ -3,34 +3,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-	/* 	const [pokemon, setpokemon] = useState([]);
+	const [pokemon, setPokemon] = useState([]);
 
-	useEffect(() => {
-		fetch("https://pokeapi.co/api/v2/pokemon/?limit=810")
-			.then((response) => response.json())
-			.then((response) => setpokemon(response.results));
-	}, []); */
-
-	const [responseData, setResponseData] = useState(null);
 	useEffect(() => {
 		axios
-			.get("https://pokeapi.co/api/v2/pokemon/?limit=810")
+			.get("https://pokeapi.co/api/v2/pokemon?limit=807")
 			.then((response) => {
-				setResponseData(response.data);
+				setPokemon(response.data.results);
+			})
+			.catch((err) => {
+				console.log(err);
 			});
 	}, []);
 
 	return (
-		<div className="App">
-			{responseData.map((aPokemon, index) => {
-				return <div key={index}>{aPokemon.name}</div>;
-			})}
-
-			{/* 	{pokemon.length > 0 &&
-				pokemon.map((aPokemon, index) => {
-					return <div key={index}>{aPokemon.name}</div>;
-				})} */}
+		<div className="App" style={{ width: "200px", margin: "auto" }}>
+			<ul>
+				{pokemon.map((pokemonObj, index) => {
+					return <li key={index}>{pokemonObj.name}</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
+
 export default App;
