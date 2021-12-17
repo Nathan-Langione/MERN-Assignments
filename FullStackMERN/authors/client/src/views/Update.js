dda9e22e4e0e46714a32e2ade915f0c0ea8965c5
Nathan Langione <nlangione@wayfair.com>
2021-12-17 12:46:06 -0500

    Everything is working pre validate

--- FullStackMERN/authors/client/src/views/Update.js
+++ FullStackMERN/authors/client/src/views/Update.js
@@ -2,25 +2,29 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { navigate } from '@reach/router';
 import AuthorForm from '../components/AuthorForm';
-import DeleteButton from '../components/DeleteButton';
+
 const Update = (props) => {
     const { id } = props;
     const [author, setAuthor] = useState();
     const [loaded, setLoaded] = useState(false);
+    const [oldName, setOldName] = useState();
+
     useEffect(() => {
         axios.get('http://localhost:8000/api/authors/' + id)
             .then(res => {
                 setAuthor(res.data);
                 setLoaded(true);
+                //setOldName(author.name);
             })
         // eslint-disable-next-line
-    }, [])
+    }, [loaded]);
+
     const updateAuthor = author => {
         axios.put('http://localhost:8000/api/authors/' + id, author)
             .then(res => {
                 console.log(res);
+                console.log("The author " + oldName + " has been updated to " + author.name);
                 navigate("/");
-                console.log("here");
             })
     }
     return (
@@ -32,9 +36,6 @@ const Update = (props) => {
                         onSubmitProp={updateAuthor}
                         initialName={author.name}
                     />
-                    <DeleteButton
-                        authorId={author._id}
-                        successCallback={() => navigate("/")} />
                 </>
             )}
         </div>
