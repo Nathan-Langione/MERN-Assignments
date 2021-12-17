74fe147dbdc01ce9c18cd8a3eb6824d825bf1393
Nathan Langione <nlangione@wayfair.com>
2021-12-17 12:24:19 -0500

    previous commit where things worked: 322af2f, changed update div

--- FullStackMERN/authors/client/src/views/Update.js
+++ FullStackMERN/authors/client/src/views/Update.js
@@ -1,41 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { navigate } from '@reach/router';
+import { Link } from '@reach/router';
 import AuthorForm from '../components/AuthorForm';
-import DeleteButton from '../components/DeleteButton';
+
 const Update = (props) => {
     const { id } = props;
     const [author, setAuthor] = useState();
     const [loaded, setLoaded] = useState(false);
+    const [oldName, setOldName] = useState();
+
     useEffect(() => {
         axios.get('http://localhost:8000/api/authors/' + id)
             .then(res => {
                 setAuthor(res.data);
+                setOldName(author.name);
                 setLoaded(true);
             })
         // eslint-disable-next-line
     }, [])
+
     const updateAuthor = author => {
         axios.put('http://localhost:8000/api/authors/' + id, author)
             .then(res => {
                 console.log(res);
+                console.log("The author " + oldName + " has been updated to " + author.name);
                 navigate("/");
-                console.log("here");
+
             })
     }
     return (
         <div>
-            <h1>Update Author</h1>
+            <h1>Favorite authors</h1>
+            <Link to={"/"}>
+                Home
+            </Link>
+            <h1>Edit this author:</h1>
             {loaded && (
-                <>
-                    <AuthorForm
-                        onSubmitProp={updateAuthor}
-                        initialName={author.name}
-                    />
-                    <DeleteButton
-                        authorId={author._id}
-                        successCallback={() => navigate("/")} />
-                </>
+                <AuthorForm
+                    onSubmitProp={updateAuthor}
+                    initialName={author.name}
+                />
             )}
         </div>
     )
