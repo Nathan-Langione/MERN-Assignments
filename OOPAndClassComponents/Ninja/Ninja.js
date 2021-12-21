0c0ab0361a89082e1dbdd38e531189469a9739df
Nathan Langione <nlangione@wayfair.com>
2021-12-20 19:24:03 -0500

    updates based on solution

--- OOPAndClassComponents/Ninja/Ninja.js
+++ OOPAndClassComponents/Ninja/Ninja.js
@@ -28,8 +28,10 @@ class Ninja {
 }
 
 class Sensei extends Ninja {
-    constructor(name, wisdom = 10) {
-        super(name, 200, 10, 10);
+    // constructor(name, wisdom = 10) {
+    // super(name, 200, 10, 10);
+    constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10) {
+        super(name, health, speed, strength);
         this.wisdom = wisdom;
     }
 
@@ -39,19 +41,20 @@ class Sensei extends Ninja {
     }
 
     speakWisdom() {
+        console.log("Speaking wisdom...");
         this.drinkSake();
         console.log("What one programmer can do in one month, two programmers can do in two months");
         return this;
     }
 }
 
-/*
+
 const ninja1 = new Ninja("Hyabusa", 100);
 ninja1.sayName().showStats().drinkSake().drinkSake().showStats()
 
 const ninja2 = new Ninja("Billy", 15, 5, 7);
 ninja2.sayName().showStats().drinkSake();
- */
+
 const sensei1 = new Sensei("Master Splinter");
 
 sensei1.speakWisdom().showStats();
